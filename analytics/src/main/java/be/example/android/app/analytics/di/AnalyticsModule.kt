af1950414258c8bbd6737c9ddc3a07eaacf8616b
Dominik Hys <dominik.hys@vodeno.com>
2022-09-13 16:22:02 +0200

    initial commit

---
+++ analytics/src/main/java/be/example/android/app/analytics/di/AnalyticsModule.kt
@@ -0,0 +1,10 @@
+package be.example.android.app.analytics.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AnalyticsModule {
+}
---
+++ app/src/main/java/be/example/android/app/app/MyApplication.kt
@@ -0,0 +1,7 @@
+package be.example.android.app.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
